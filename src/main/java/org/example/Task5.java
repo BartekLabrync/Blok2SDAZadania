8d17721c3fbb17a462cb66e11016e76b43e11c07
BartekLabrync <barteklabrync7@gmail.com>
2023-01-21 14:40:28 +0100

    statement t5

---
+++ src/main/java/org/example/Task5.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int firstDigit = 4;
+        int secondDigit = 11;
+        int result = sum(firstDigit, secondDigit);
+        System.out.println("result:" + result);
+    }
+
+    private static int sum(int firstDigit, int secondDigit) {
+        int sum = 0;
+        /*for (int i = firstDigit; i <= secondDigit; i++){
+            sum += i;
+        }
+        return sum;
+
+
+
+        while (firstDigit <= secondDigit){
+            sum =+firstDigit;
+            firstDigit++;
+        }
+        return sum;
+        */
+
+
+    }
+
+}
+
+
+
